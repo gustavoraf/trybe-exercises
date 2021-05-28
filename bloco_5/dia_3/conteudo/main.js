4462458a2e0d81ded91bac99246d7dbfa4484c48
Gustavo <axt872@gmail.com>
2021-05-28 16:45:52 -0300

    conteudo do dia 3 bloco 5

---
+++ bloco_5/dia_3/conteudo/main.js
@@ -0,0 +1,66 @@
+const divUm = document.getElementById('divUm');
+const divDois = document.getElementById('divDois');
+const divTres = document.getElementById('divTres');
+const input = document.getElementById('input');
+const myWebpage = document.getElementById('mySpotrybefy');
+
+function addClass(element) {
+  document.getElementsByClassName("tech")[0].className = ""
+  element.target.className = "tech"
+}
+
+divDois.addEventListener("click", addClass)
+divTres.addEventListener("click", addClass)
+divUm.addEventListener("click", addClass)
+
+function changeTech(content) {
+  document.getElementsByClassName("tech")[0].innerText = content.target.value
+}
+
+input.addEventListener("keyup", changeTech)
+
+function redirect(elemento) {
+  location.href = "https://www.google.com"
+  console.log("deu")
+}
+
+myWebpage.addEventListener("dblclick", redirect)
+
+function colorChange() {
+  myWebpage.style.color = "green"
+}
+
+function colorChangeBack() {
+  myWebpage.style.color = "white"
+}
+
+myWebpage.addEventListener("mouseover", colorChange)
+myWebpage.addEventListener("mouseleave", colorChangeBack)
+
+
+/*
+ Copie esse arquivo e edite apenas ele;
+ Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+ Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+com a classe 'tech';
+ Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+redirecione para alguma página;
+1. Que tal redirecionar para seu portifólio?
+ Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+a cor do mesmo;
+
+Segue abaixo um exemplo do uso de event.target:
+*/
+
+function resetText(event) {
+  // O Event é passado como um parâmetro para a função.
+  event.target.innerText = 'Opção reiniciada';
+  // O event possui várias propriedades, porém a mais usada é o event.target,
+  // que retorna o objeto que disparou o evento.
+}
+
+divUm.addEventListener('dblclick', resetText);
+// Não precisa passar o parâmetro dentro do addEventListener. O próprio
+// navegador fará esse trabalho por você, não é legal? Desse jeito, o
+// event.target na nossa função retornará o objeto 'divUm'.
